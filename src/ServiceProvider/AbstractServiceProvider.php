7188daa47c816c583b32ecb6aefc3ed285ce9b79
omegacms <ospress@outlook.it>
2024-08-31 17:53:24 +0200

    Begin Code Review and Correction Phase with PHPStan
    Initiated the process of code review and correction to enhance code quality and ensure adherence to coding standards. PHPStan, a static analysis tool for PHP, has been integrated to identify potential issues, code smells, and areas requiring improvement. This phase focuses on addressing any reported errors, warnings, and recommendations generated by PHPStan to ensure that the codebase is robust, maintainable, and free of potential bugs.
    
    Key actions:
    - Integrated PHPStan for static code analysis.
    - Began reviewing and correcting code based on PHPStan findings.
    - Ensured compliance with coding standards and best practices.
    
    This commit marks the start of an ongoing effort to improve code quality through systematic analysis and correction.

--- src/ServiceProvider/AbstractServiceProvider.php
+++ src/ServiceProvider/AbstractServiceProvider.php
@@ -4,14 +4,14 @@
  *
  * @link       https://omegacms.github.io
  * @author     Adriano Giovannini <omegacms@outlook.com>
- * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @copyright  Copyright (c) 2024 Adriano Giovannini. (https://omegacms.github.io)
  * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  */
 
 /**
  * @declare
  */
-declare(strict_types=1);
+declare( strict_types = 1 );
 
 /**
  * @namespace
@@ -36,7 +36,7 @@ use RuntimeException;
  * @package     Omega\ServiceProvider
  * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
- * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @copyright   Copyright (c) 2024 Adriano Giovannini. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
